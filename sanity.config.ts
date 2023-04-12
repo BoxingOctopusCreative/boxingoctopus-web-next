8879aef7ca0de6a41eb48822f3ea7f788ca5813f
Ryan Draga <ryan.draga@boxingoctop.us>
2023-04-12 17:21:55 -0400

    Adding plugins to Sanity Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import schemas from "./sanity/schemas";
+import { defineConfig } from "sanity"
+import { deskTool } from "sanity/desk"
+import { media } from "sanity-plugin-media"
+import { scheduledPublishing } from "@sanity/scheduled-publishing"
+import {codeInput} from '@sanity/code-input'
+import { markdownSchema } from "sanity-plugin-markdown"
+import schemas from "./sanity/schemas"
 
 const config = defineConfig({
     projectId:  "soan4bom",
@@ -8,7 +12,13 @@ const config = defineConfig({
     title:      "Boxing Octopus Creative",
     apiVersion: "2023-04-07",
     basePath:   "/admin",
-    plugins:    [deskTool()],
+    plugins:    [
+        deskTool(),
+        media(),
+        scheduledPublishing(),
+        codeInput(),
+        markdownSchema(),
+    ],
     schema:     { types: schemas },
 });
 
